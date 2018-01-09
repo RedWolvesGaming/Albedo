abce4b6b66724aa50045fd9f61496c8906a1c283
Rastaphel <35265917+Rastaphel@users.noreply.github.com>
2018-01-09 15:15:01 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,14 @@
+   "done": true
+    },
+    "roll": {
+ -    "text": "Rolls the dice a number of times with a number of sides, see **`!random`** for advanced generators",
+ +    "text": "Rolls a die a number of times with a number of sides, see **`!random`** for advanced generators",
+      "done": true
+    },
+    "serverinfo": {
+ @@ -647,4 +647,4 @@
+      "text": "Usage: **`!youtube`** `video tags`",
+      "done": true
+    }
+ -} 
+ +}
